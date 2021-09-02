32ebb04604ab376b738ff41b6372dc260922be34
Leo Shin <54517654+Shin-Leo@users.noreply.github.com>
2021-09-02 14:23:59 -0700

    initial commit

---
+++ src/app/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+import imports "github.com/rocketlaunchr/dataframe-go/imports"
+
+func readCSVFromUrl(url string) ([][]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	reader := csv.NewReader(resp.Body)
+	reader.Comma = ';'
+	data, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func main() {
+	ctx := context.Background()
+	dataCSV, err := readCSVFromUrl("https://raw.githubusercontent.com/fantasydatapros/data/master/yearly/2020.csv")
+	if err != nil {
+		panic(err)
+	}
+	var csvString = ""
+	i := 0
+	for i < len(dataCSV) {
+		var row = strings.Join(dataCSV[i], ",")
+		row += "\n"
+		csvString += row
+		i += 1
+	}
+	df, err := imports.LoadFromCSV(ctx, strings.NewReader(csvString))
+	fmt.Println(df)
+}
